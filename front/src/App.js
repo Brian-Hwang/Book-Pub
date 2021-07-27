0010859fc4c81727e53f75c3b953a049c1b2ca26
0909oje <0909oje@naver.com>
2021-07-27 14:41:30 +0900

    페이지 2개 제외 삭제

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,5 @@
-
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage/MainPage';
-import EditPage from './pages/EditPage/EditPage';
-import KustomPage from './pages/KustomPage/KustomPage';
 import ViewerPage from './pages/ViewerPage/ViewerPage';
 
 function App() {
@@ -10,8 +7,6 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={MainPage} />
-        <Route exact path="/edit" component={EditPage} />
-        <Route exact path="/kustom" component={KustomPage} />
         <Route exact path="/viewer" component={ViewerPage} />
       </Switch>
     </Router>
